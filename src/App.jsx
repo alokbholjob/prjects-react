872697777850d6eef3c5305bab83791fb7ab7f36
Alok Kumar Bhol <kalokbhol77@gmail.com>
2024-05-13 10:57:49 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-4xl text-center'>Realtor React Application</h1>
+      <p>Build with Vite and tailwind css</p>
+    </>
+  )
+}
+
+export default App;
